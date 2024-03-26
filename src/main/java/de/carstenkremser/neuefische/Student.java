890820671136b28cd330ff5438431bd5e168f38c
Carsten Kremser <github@carstenkremser.de>
2024-03-26 15:01:38 +0100

    Add Lombok annotations to class Student

--- src/main/java/de/carstenkremser/neuefische/Student.java
+++ src/main/java/de/carstenkremser/neuefische/Student.java
@@ -1,8 +1,14 @@
 package de.carstenkremser.neuefische;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
 public class Student {
     int id;
     String name;
